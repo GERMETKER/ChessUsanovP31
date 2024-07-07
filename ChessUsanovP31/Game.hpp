3cd18a003e3bab2b64f0ae46ff2143ec330bd1f5
GERMETKER <160734087+GERMETKER@users.noreply.github.com>
2024-07-07 09:32:56 +0500

    Add files via upload

---
+++ ChessUsanovP31/Game.hpp
@@ -0,0 +1,17 @@
+#ifndef _GAME_HPP_
+#define _GAME_HPP_
+#include <SFML/Graphics.hpp>
+using namespace sf;
+class Game
+{
+public:
+	Game()
+	{
+		CrChessDesk();
+	}
+	void CrChessDesk();
+
+private:
+	
+};
+#endif
